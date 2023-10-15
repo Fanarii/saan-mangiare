a3fdd9fa9b21ce8bb7bdbc1759ffedea68a432f3
Sigit Fajar Desfian <sigitfajard@gmail.com>
2023-10-15 16:12:09 +0700

    caching and webmanifest

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  CACHE_NAME: 'Saan-Mangiare-V2.1-' + Date.now(),
+  DATABASE_NAME: 'restaurant-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurant'
+}
+
+export default CONFIG
